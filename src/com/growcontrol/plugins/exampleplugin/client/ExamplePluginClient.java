f25bacd75cb577fc500958a08d9ff972b71c4736
lorenzop <lorenzo@poixson.com>
2017-06-03 13:59:11 -0400

    added ExamplePluginClient class (mostly empty)

---
+++ src/com/growcontrol/plugins/exampleplugin/client/ExamplePluginClient.java
@@ -0,0 +1,27 @@
+package com.growcontrol.plugins.exampleplugin.client;
+
+import com.growcontrol.client.plugins.gcClientPlugin;
+import com.poixson.app.plugin.xPluginManager;
+import com.poixson.app.plugin.xPluginYML;
+
+
+public class ExamplePluginClient extends gcClientPlugin {
+
+
+
+	public ExamplePluginClient(final xPluginManager<?> manager, final xPluginYML yml) {
+		super(manager, yml);
+	}
+
+
+
+	@Override
+	protected void onEnable() {
+	}
+	@Override
+	protected void onDisable() {
+	}
+
+
+
+}
